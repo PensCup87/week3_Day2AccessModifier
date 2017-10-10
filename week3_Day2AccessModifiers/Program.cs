5c19a319067f8158f1d9e33ee51a2adc5e1ca539
PensCup87 <bdt9123@gmail.com>
2017-10-10 09:45:05 -0400

    Initial Commit

---
+++ week3_Day2AccessModifiers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3_Day2AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
