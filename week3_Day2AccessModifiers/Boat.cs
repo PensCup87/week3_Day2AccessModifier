5cc06a4aaa6b5b670695e2147df942e952fd528c
PensCup87 <bdt9123@gmail.com>
2017-10-10 13:04:07 -0400

    Finished Access Mofifires

---
+++ week3_Day2AccessModifiers/Boat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3_Day2AccessModifiers
+{
+    class Boat
+    {
+        //these look like variables, but they are FIELDS (90% of time FIELDS ARE PRIVATE)
+        //fields are characteristics or class information
+        //information about the specific instance
+        public int numberOfPassengers;
+        
+        private string name;
+        private double engineSize;
+
+
+        //this is the Property - used to expose private fields
+        //PROPERTIES ARE PUBLIC SO THAT DATA CAN BE UTILIZED
+        //Property Advantage - can hide where information is stored(good for storing passwords)
+        public string Name
+        {
+            get { return name; }//get = 
+            set { name = value; }//set = 
+        }
+
+        //This is the Default Constructor
+        //has Enpty ()
+        //Constructors are used to Create Something
+        //The Default Constructor Does Not have to be used
+        //All Constructors Initialize an Object
+        //CONSTRUCTORS HAVE TO BE PUBLIC SO THEY CAN BE CALLED
+        public Boat()
+        {
+
+        }
+
+        //adding A New Constructor
+        public Boat(int numberOfPassengers, string name, double engineSize)
+        {
+            //these are the instance variables
+            this.numberOfPassengers = numberOfPassengers;
+            this.name = name;
+            this.engineSize = engineSize;
+        }
+
+
+        //Move and Calculate Are Methods
+        //Move Will not Reurn a Value
+        //Move Can Be accessed Anywhere
+        public void Move()
+        {
+            Console.WriteLine(name + " moves forward.");
+            //code to make the boat move
+        }
+
+
+        //Calculate Can only be accessed within this Class
+        private double CalculateMPG()
+        {
+            //code to calsulate the MPG - example calculation
+            double MPG = engineSize * .7d;
+            return MPG; 
+        }
+
+        //MAKING A METHOD TO USE THE MPG METHOD
+        public void BoatInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Passengers: " + numberOfPassengers);
+            Console.WriteLine("Engine Size: " + engineSize);
+            //Method within a Method & The method is Private
+            Console.WriteLine("MPG: " + CalculateMPG());
+            Console.WriteLine();//adds blank line
+        }
+    }
+}
--- week3_Day2AccessModifiers/Program.cs
+++ week3_Day2AccessModifiers/Program.cs
@@ -10,6 +10,20 @@ namespace week3_Day2AccessModifiers
     {
         static void Main(string[] args)
         {
+            //Creates a new Instance with the Constructor with 3 arguments
+            Boat Luca = new Boat(3, "Luca", 18.6);
+            Luca.Move();
+
+
+            //eventhough the Methos was created, C# does not recognize this
+            //its private and the program does not recognize it
+            //Luca.CalculateMPG();
+
+            Luca.BoatInfo();
+
+            //creating a new instance
+            Boat boat2 = new Boat(14, "Pedro", 30.8);
+            boat2.BoatInfo();
         }
     }
 }
